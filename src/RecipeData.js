3e9bed8ef98c6b8d251061526baabe9d314c87d4
Britt-H <brittain.hoover@gmail.com>
2023-04-21 12:21:43 -0400

    initial push

---
+++ src/RecipeData.js
@@ -0,0 +1,23 @@
+/**
+* This is the data that is initially loaded.
+* Please do not modify this file.
+*/
+
+const RecipeData = [
+  {
+    name: "Tuna Poke with Mango",
+    cuisine: "Hawaiian",
+    photo: "https://www.ohow.com/wp-content/uploads/2021/08/iStock-1045650252-copy.jpg",
+    ingredients: "1 package of sushi grade tuna. 1 cup cooked quinoa ½ avocado, sliced. ½ mango, cubed. 1 shredded carrot. 1 small sliced cucumber. poke sauce.",
+    preparation: "1. Chop tuna into cubes. 2. Toss with 1 tbsp sesame oil and 1 tbsp tamari. Set aside. 3. Layer your poke bowl starting with quinoa. 4. Whisk all poke sauce ingredients in a bowl and pour over the poke bowl. 5. Garnish with sesame seeds and furikake. 6. Top with chopped scallions."
+  },
+  {
+    name: "Guacamole",
+    cuisine: "Mexican",
+    photo: "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2019/04/Guacamole-1-2.jpg",
+    ingredients: "3 ripe avocados. ¾ cup finely chopped Roma tomato. 2 serrano chiles very finely chopped (seeded and deveined). 3 heaping tablespoons of finely chopped onion. 3 tablespoons of minced cilantro. Salt to taste",
+    preparation: "1. Remove the flesh of the avocados. 2. Mash the avocados with the back of a fork. 3. Add the other ingredients and incorporate evenly. 4. Add salt to taste."
+  },
+];
+
+export default RecipeData;
